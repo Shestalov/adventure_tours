c411b49dac9934398649fd4abbb302b106dd71be
Alex <alexshestalov@gamil.com>
2022-09-27 19:26:40 +0300

    added views

--- route/views.py
+++ route/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def add_route(request):
+    return HttpResponse("<h1>Add route</h1>")
+
+
+def detail_route(request, id_route):
+    return HttpResponse("<h1>Detail route</h1>")
+
+
+def add_event_route(request, id_route):
+    return HttpResponse("<h1>Add event</h1>")
+
+
+def filter_route(request, route_type=None, country=None, location=None):
+    return HttpResponse("<h1>Filter route</h1>")
+
+
+def review_route(request, id_route):
+    return HttpResponse("<h1>Reviews route</h1>")
+
+
+def event_handler(request):
+    return HttpResponse("<h1>Event handler</h1>")
