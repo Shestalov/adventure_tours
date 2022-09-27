b3fcfc3959495d432a69ff00de1a4a963c4cbec1
Alex <alexshestalov@gamil.com>
2022-09-27 20:43:07 +0300

    added index

--- route/views.py
+++ route/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 
 
+# temporary, main page adventure tours
+def index(request):
+    return HttpResponse("<h1>OK</h1>")
+
+
+def filter_route(request, route_type=None, country=None, location=None):
+    return HttpResponse("<h1>routes</h1>")
+
+
 def add_route(request):
     return HttpResponse("<h1>Add route</h1>")
 
@@ -9,17 +18,9 @@ def detail_route(request, id_route):
     return HttpResponse("<h1>Detail route</h1>")
 
 
-def add_event_route(request, id_route):
-    return HttpResponse("<h1>Add event</h1>")
-
-
-def filter_route(request, route_type=None, country=None, location=None):
-    return HttpResponse("<h1>Filter route</h1>")
-
-
 def review_route(request, id_route):
     return HttpResponse("<h1>Reviews route</h1>")
 
 
-def event_handler(request):
-    return HttpResponse("<h1>Event handler</h1>")
+def add_event_route(request, id_route):
+    return HttpResponse("<h1>Add event</h1>")
